41b89d8a15b6b664d2f9aa473e3e0ee04808aa40
2791034692 <2791034692@qq.com>
2022-08-31 14:05:08 +0800

    1

--- common/src/main/java/net/t1y/cloud2/common/service/T1Service.kt
+++ common/src/main/java/net/t1y/cloud2/common/service/T1Service.kt
@@ -1,7 +1,8 @@
 package net.t1y.cloud2.common.service
 
 open class T1Service {
-    fun openKotlinGlobalScope(b:Boo){
-
+    private var _kotlinGlobalScope:Boolean = false
+    fun openKotlinGlobalScope(b:Boolean){
+        this._kotlinGlobalScope = b
     }
 }
