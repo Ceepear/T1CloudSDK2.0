1b43a7ba57769c2a1912445776d13329a7fe4172
2791034692 <2791034692@qq.com>
2022-09-02 12:55:27 +0800

    2

--- common/src/main/java/net/t1y/cloud2/common/service/T1Service.kt
+++ common/src/main/java/net/t1y/cloud2/common/service/T1Service.kt
@@ -1,12 +1,21 @@
 package net.t1y.cloud2.common.service
 import net.t1y.cloud2.common.os.Message
+import net.t1y.cloud2.common.os.T1CloudCtrl
+
 abstract class T1Service{
+    private lateinit var cloud:T1CloudCtrl
     fun onStart(){
 
     }
     abstract fun handleMessage(msg:Message)
     fun sendMessage(msg: Message){
         handleMessage(msg)
+    }
+    fun getServiceManger(): T1CloudCtrl {
+        return cloud
+    }
+    fun setT1ServiceManger(cloud:T1CloudCtrl){
+        this.cloud = cloud;
     }
 
     fun onStop(){}
